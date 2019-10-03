5137bf2077a7ec1e3b1574c98ef9b17ed6b9768b
salas <salas@M521.usbcali.edu.co>
2019-10-02 19:21:43 -0500

    7

---
+++ bank-logic/src/main/java/co/edu/usbcali/bank/repository/Impl/ClienteRepositoryImpl.java
@@ -0,0 +1,41 @@
+package co.edu.usbcali.bank.repository.Impl;
+
+import java.util.List;
+import java.util.Optional;
+
+import co.edu.usbcali.bank.domain.Cliente;
+import co.edu.usbcali.bank.repository.ClienteRepository;
+
+public class ClienteRepositoryImpl implements ClienteRepository {
+
+	@Override
+	public Cliente save(Cliente entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Cliente> findById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Cliente> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Cliente entity) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
