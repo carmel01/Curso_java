260886508fde440e232171b2e05d00e0820ac3e2
salas <salas@M520.usbcali.edu.co>
2019-10-08 18:22:38 -0500

    o

--- bank-web/src/main/java/co/edu/usbcali/bank/controller/ClienteController.java
+++ bank-web/src/main/java/co/edu/usbcali/bank/controller/ClienteController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,33 +16,41 @@ import co.edu.usbcali.bank.dto.ClienteDTO;
 import co.edu.usbcali.bank.mapper.ClienteMapper;
 import co.edu.usbcali.bank.repository.ClienteService;
 
-
 @RestController
 @RequestMapping("/cliente")
 public class ClienteController {
 
 	@Autowired
 	ClienteService clienteService;
-	
-	@Autowired 
+
+	@Autowired
 	ClienteMapper clienteMapper;
-	
+
+	@PostMapping("/save")
+	public ClienteDTO save(@RequestBody ClienteDTO clienteDTO) {
+		try {
+			Cliente cliente = clienteMapper.clienteDTOtoCliente(clienteDTO);
+			cliente = clienteService.save(cliente);
+			return clienteMapper.clienteToClienteDTO(cliente);
+		} catch (Exception e) {
+			return null;
+		}
+	}
+
 	@GetMapping("/findById/{id}")
 	public ClienteDTO findById(@PathVariable("id") Long id) {
-		Optional<Cliente> clienteOptional=clienteService.findById(id);
-		if(clienteOptional.isPresent()==false)
-		{
-		return null;
+		Optional<Cliente> clienteOptional = clienteService.findById(id);
+		if (clienteOptional.isPresent() == false) {
+			return null;
 		}
-		Cliente cliente=clienteOptional.get();
+		Cliente cliente = clienteOptional.get();
 		return clienteMapper.clienteToClienteDTO(cliente);
 	}
-	
+
 	@GetMapping("/findAll")
-	public List<ClienteDTO> findAll()	
-	{
-		List<Cliente> listaClientes =clienteService.findAll();
-		List<ClienteDTO> listaClientesDTO =clienteMapper.toClientesDTO(listaClientes);
+	public List<ClienteDTO> findAll() {
+		List<Cliente> listaClientes = clienteService.findAll();
+		List<ClienteDTO> listaClientesDTO = clienteMapper.toClientesDTO(listaClientes);
 		return listaClientesDTO;
 	}
 }
