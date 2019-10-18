e78bbf78edbded7dbc961bfa43ea7d9ef588aa93
salas <salas@M520.usbcali.edu.co>
2019-10-17 20:38:16 -0500

    final final de banco

--- bank-entity-boot/src/main/java/co/edu/usbcali/bank/controller/ClienteController.java
+++ bank-entity-boot/src/main/java/co/edu/usbcali/bank/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import co.edu.usbcali.bank.service.ClienteService;
 
 @RestController
 @RequestMapping("/cliente")
+@CrossOrigin("*")
 public class ClienteController {
 
 	private final static Logger log = LoggerFactory.getLogger(ClienteController.class);
